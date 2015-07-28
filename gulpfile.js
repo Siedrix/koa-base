61f2f4be827a8f6a8382ed1543dc737958801b87
Siedrix <Siedrix@gmail.com>
2015-07-28 16:57:41 -0500

    optimize webpack

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,10 @@
+var _ = require('underscore');
+var path = require('path');
+
 var gulp = require('gulp');
 var gutil = require('gulp-util');
-var rename = require("gulp-rename");
+var rename = require('gulp-rename');
+var CompressionPlugin = require('compression-webpack-plugin');
 
 var webpack  = require('webpack');
 
@@ -12,25 +16,73 @@ gulp.task('reload', function (callback) {
 	return gulp.src('frontend/main.jsx').pipe( livereload() );
 });
 
+var node_modules_dir = path.join(__dirname, 'node_modules');
+var webpackConfig = {
+	entry: './frontend/main.jsx',
+	output: {
+		filename: 'public/js/build/main.js',
+		sourceMapFilename:'[file].map'
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.jsx?$/,
+				exclude: /(node_modules|bower_components)/,
+				loader: 'babel'
+			}
+		],
+	}
+}
+
+webpackConfig.resolve = {alias: {}};
+webpackConfig.module.noParse = [];
+
+var deps = [
+	'jquery/dist/jquery.min.js',
+	'react/dist/react.min.js',
+	'moment/min/moment.min.js',
+	'underscore/underscore-min.js',
+];
+
+deps.forEach(function (dep) {
+	var depPath = path.resolve(node_modules_dir, dep);
+	webpackConfig.resolve.alias[dep.split(path.sep)[0]] = depPath;
+	webpackConfig.module.noParse.push(depPath);
+});
+
 gulp.task("webpack", function(callback) {
 	// run webpack
-	webpack({
-		entry: './frontend/main.jsx',
-		output: {
-			filename: 'public/js/build/main.js',
-			sourceMapFilename:'[file].map'
-		},
-		devtool: "source-map",
-		module: {
-			loaders: [
-				{
-					test: /\.jsx?$/,
-					exclude: /(node_modules|bower_components)/,
-					loader: 'babel'
-				}
-			],
-		}
-	}, function(err, stats) {
+	var configClone = _.extend({}, webpackConfig);
+	configClone.devtool = ["source-map"];
+
+	webpack(configClone, function(err, stats) {
+		if(err) throw new gutil.PluginError("webpack", err);
+		var statsAsString = stats.toString({});
+
+		gutil.log("[webpack]", statsAsString.split('chunk')[0]);
+		// livereload({ start: true })
+		// console.log('Should reload', livereload());
+		livereload.changed('public/js/main.js');
+		callback();
+	});
+});
+
+// Production ready files
+gulp.task("build", function(callback) {
+	// run webpack
+	var configClone = _.extend({}, webpackConfig);
+
+	configClone.plugins = [];
+	configClone.plugins.push( new webpack.optimize.UglifyJsPlugin({minimize: true}) );
+	configClone.plugins.push( new CompressionPlugin({
+		asset: "{file}.gz",
+		algorithm: "gzip",
+		regExp: /\.js$|\.html$/,
+		threshold: 500,
+		minRatio: 0.8
+	}) );
+
+	webpack(configClone, function(err, stats) {
 		if(err) throw new gutil.PluginError("webpack", err);
 		var statsAsString = stats.toString({});
 
